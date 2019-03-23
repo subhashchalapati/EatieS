e90aed7ffaa4a8ec937b8858538dc1db8c2a0555
subhash chalapati <subhashchalapati21@gmail.com>
2019-03-23 18:13:27 +0530

    6th commit (I think)
    added some new pages, some jquary(with backup) added header and footer pages and they can be included

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -4,18 +4,17 @@
  * and open the template in the editor.
  */
 function on() {
-    console.log("on")
+    console.log("on");
   document.getElementById("Homeoverlay").style.display = "block";
 }
 function off() {
+    console.log("off");
   document.getElementById("Homeoverlay").style.display = "none";
 }
 
-
-
 //function SlideImage() {
 var slideIndex = 1;
-showDivs(slideIndex);
+showDivs(slideIndex);3
 
 
 //function AutoAddSlide() {
@@ -52,11 +51,29 @@ function showDivs(n) {
         dots[j].style.backgroundColor="transparent";
         }
     dots[slideIndex-1].style.backgroundColor="white";
-        
+      
 }
 //setTimeout(AutoAddSlide, 4000);
 //}
 
 
+//
+////var one = MainHed.innerHTML;
+//
+//
+//var MainHed = document.getElementById('head');
+//var AboutHed = document.getElementById('Aboutehead');
+function copy(){
+    console.log("hello About");
+                                    //var MainHed = document.getElementById("head");
+                                    //var AboutHed = document.getElementById("Aboutehead");
+                                    //AboutHed.innerHTML = MainHed.innerHTML;
+ var itm = document.getElementById("AboutHed").lastChild;
+  var cln = itm.cloneNode(true);
+  document.getElementById("MainHed").appendChild(cln);
+//    console.log(MainHed.innerHTML);
+    console.log("hello about 2");
+
+}
 
 
