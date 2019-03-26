65012a5f3b2fe259b2f5d63378fdb3df6724bc18
subhash chalapati <subhashchalapati21@gmail.com>
2019-03-26 15:03:29 +0530

    for vishnu

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//This on and off is for overlay card
 function on() {
     console.log("on");
   document.getElementById("Homeoverlay").style.display = "block";
@@ -11,6 +12,42 @@ function off() {
     console.log("off");
   document.getElementById("Homeoverlay").style.display = "none";
 }
+
+
+
+
+
+
+function OfferSlideImages(){
+//this is for loding slideimages
+    // var src = documtnt.getpropertyByClassName("mySlides").
+var src = document.getElementsByClassName("mySlides");
+//console.log(src[0].getAttribute("src"));
+console.log(src.length);
+
+
+for (var ittr = 0; ittr < src.length; ittr++) {
+
+    var x = ittr+1 ;
+    var i = "img/img"+x+".jpeg";
+    var ass = src[ittr].setAttribute("src", i); 
+    console.log(src[ittr].getAttribute("src"));
+//console.log(i); //    console.log(ittr+1); //    console.log(i);
+}
+
+var js = $.getJSON("/index.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+var myObj = JSON.parse(js);
+document.getElementById("demo").innerHTML = myObj.name;
+
+
+
+
+}
+
+
+
 
 //function SlideImage() {
 var slideIndex = 1;
